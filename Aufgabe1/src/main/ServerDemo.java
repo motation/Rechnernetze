e49cfe7ca32da379ed4c36a2f906611b80e36339
ole <ole@Seneca>
2013-10-25 16:02:16 +0200

    update

--- Aufgabe1/src/main/ServerDemo.java
+++ Aufgabe1/src/main/ServerDemo.java
@@ -1,14 +1,8 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Properties;
 
