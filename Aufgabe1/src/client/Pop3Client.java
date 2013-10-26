12f2ef3e3bd2c36de0b8658c20d0af9dfd57bfe3
sven-ole.fedders <sven-ole.fedders@haw-hamburg.de>
2013-10-26 14:47:54 +0200

    opimized code

--- Aufgabe1/src/client/Pop3Client.java
+++ Aufgabe1/src/client/Pop3Client.java
@@ -3,6 +3,7 @@ package client;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,6 +12,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,91 +20,103 @@ public class Pop3Client implements Runnable {
 	private List<User> users;
 	private Socket socket;
 	private File sessionProtocol;
+	private Properties prop = new Properties();
+	private FileWriter sessionWriter;
+	private BufferedWriter writer;
+	BufferedReader reader;
 
 	public Pop3Client(List<User> users) {
 		this.users = users;
+		try {
+			// load a properties file and absolute dir
+			prop.load(new FileInputStream(System.getProperty("user.dir")
+					+ "/src/config/auth.properties"));
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 
 	public void fetchMails() throws UnknownHostException, IOException {
 		for (User user : users) {
 			this.socket = new Socket(user.getServer(), user.getPort());
-			// log dir
-			this.sessionProtocol = new File("D:/Mail/logs/" + user.getUser());
 
-			FileWriter fw = new FileWriter(sessionProtocol, true);
+			// log dir
+
+			this.sessionProtocol = new File(prop.getProperty("windowsDir")
+					+ "logs/" + user.getUser());
+
+			// write in exist file
+			sessionWriter = new FileWriter(sessionProtocol, true);
 
 			// Single point of control
-			String username = "USER " + user.getUser() + System.lineSeparator();
-			String password = "PASS " + user.getPass() + System.lineSeparator();
-			
+			String username = "USER " + user.getUser();
+			String password = "PASS " + user.getPass();
 
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
+			reader = new BufferedReader(new InputStreamReader(
 					socket.getInputStream()));
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+			writer = new BufferedWriter(new OutputStreamWriter(
 					socket.getOutputStream()));
 
 			// login and protocol
-			fw.write(getCurrentTime() + reader.readLine() + System.lineSeparator());
-			fw.flush();
-			fw.write(getCurrentTime() + username);
-			fw.flush();
-			writer.write(username);
-			writer.flush();
-			fw.write(getCurrentTime() + reader.readLine() + System.lineSeparator());
-			fw.flush();
-			fw.write(getCurrentTime() + password);
-			fw.flush();
-			writer.write(password);
-			writer.flush();
+			writeSession(readFromServer());
+
+			writeSession(username);
+
+			writeToServer(username);
+
+			writeSession(readFromServer());
+
+			writeSession(password);
+
+			writeToServer(password);
 
 			// parse count of mails
-			String mails = reader.readLine();
-			Matcher match = Pattern.compile("\\w*\\s(\\d*)\\s").matcher(mails);
+			String numberOfMails = readFromServer();
+			Matcher match = Pattern.compile("\\w*\\s(\\d*)\\s").matcher(
+					numberOfMails);
 			int count = 0;
 			while (match.find()) {
 				count = Integer.parseInt(match.group(1));
 			}
 
-			fw.write(getCurrentTime() + mails + System.lineSeparator());
-			fw.flush();
+			writeSession(numberOfMails);
 
-			// TODO fetch mails
+			// fetch mails
 			for (int i = 1; i <= count; i++) {
-				writer.write("RETR "+i+System.lineSeparator());
-				writer.flush();
-				
-				// create dir and file
-				File mail = new File("D:/Mail/mails/"+user.getService());
+
+				writeToServer("RETR " + i);
+
+				// create dir and file in own filesystem
+				File mail = new File(prop.getProperty("windowsDir") + "mails/"
+						+ user.getService());
 				mail.setExecutable(true);
 				mail.setWritable(true);
 				mail.mkdir();
-				mail = new File("D:/Mail/mails/"+ user.getService()+"/"+i);
-				
-				
-				FileWriter fwriter = new FileWriter(mail);
-				String string = reader.readLine();
-				while((string=reader.readLine()) != null && !(string.contains(".") && string.length() == 1)){
-					
-					fwriter.write(string+System.lineSeparator());
-					fwriter.flush();
+				mail = new File(prop.getProperty("windowsDir") + "mails/"
+						+ user.getService() + "/" + i);
+
+				// looping to get mail content till single "."
+				FileWriter fileWriter = new FileWriter(mail);
+				String string = readFromServer();
+				while ((string = readFromServer()) != null
+						&& !(string.contains(".") && string.length() == 1)) {
+
+					fileWriter.write(string + System.lineSeparator());
+					fileWriter.flush();
 				}
-				fwriter.write(".");
-				
-				
+				fileWriter.write(".");
+				fileWriter.close();
+
 			}
-			
+
 			// quit connection to mail service
-			fw.write(getCurrentTime() + "QUIT " + System.lineSeparator());
-			fw.flush();
-			writer.write("QUIT " + System.lineSeparator());
-			writer.flush();
-			fw.write(getCurrentTime() + reader.readLine() + System.lineSeparator());
-			fw.write("----------------------------------------------------"
-					+ System.lineSeparator());
-			fw.flush();
+			writeSession("QUIT ");
+			writeToServer("QUIT ");
+			writeSession(readFromServer());
+			writeSession("----------------------------------------------------");
 
 			// Connection close
-			fw.close();
+			sessionWriter.close();
 			writer.close();
 			reader.close();
 			socket.close();
@@ -126,9 +140,24 @@ public class Pop3Client implements Runnable {
 		}
 
 	}
-	
-	private String getCurrentTime(){
+
+	private String getCurrentTime() {
 		Calendar cal = Calendar.getInstance();
 		return "[" + cal.getTime().toString() + "] ";
+	}
+
+	private void writeSession(String request) throws IOException {
+		sessionWriter
+				.write(getCurrentTime() + request + System.lineSeparator());
+		sessionWriter.flush();
+	}
+
+	private void writeToServer(String request) throws IOException {
+		writer.write(request + System.lineSeparator());
+		writer.flush();
+	}
+
+	private String readFromServer() throws IOException {
+		return reader.readLine();
 	}
 }
